46fd560e421bded86ee75fe82e38fe23516a9913
Elisabeteromana <133717204+Elisabeteromana@users.noreply.github.com>
2024-04-23 10:45:52 -0300

    Parte do Create e o Edit

--- mf-dev-backend-2025/Controllers/VeiculosController.cs
+++ mf-dev-backend-2025/Controllers/VeiculosController.cs
@@ -39,5 +39,34 @@ namespace mf_dev_backend_2025.Controllers
 
             return View(veiculo);
         }
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+                return NotFound();
+
+            var dados = await _context.Veiculos.FindAsync(id);
+
+            if (dados == null)
+                return NotFound();
+
+            return View(dados);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id, Veiculo veiculo)
+        {
+            if (id != veiculo.Id)
+            return NotFound();
+
+            if (ModelState.IsValid)
+            {
+                _context.Veiculos.Update(veiculo);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+
     }
  }
